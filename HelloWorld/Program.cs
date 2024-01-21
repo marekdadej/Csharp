69c64b4e0076a6752e5b414ed0c3b183ccbad440
Marek Dadej <marek.dadej03@gmail.com>
2024-01-21 13:35:31 +0100

    add programs

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proszę wpisz swoje imię:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Witaj, " + name + "!");
+        }
+    }
+}
